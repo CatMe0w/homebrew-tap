3075e6eecce51308eda1edf3c9724577d98834c5
CatMe0w <CatMe0w@live.cn>
2024-07-29 14:20:44 -0600

    zouxian v0.2.0

--- Formula/zouxian.rb
+++ Formula/zouxian.rb
@@ -1,8 +1,8 @@
 class Zouxian < Formula
-  desc "Permanent Xcode Predictive Code Completion for Chinese-market Mac computers"
+  desc "Permanent Apple Intelligence + Xcode Predictive Code Completion for Chinese-market Mac computers"
   homepage "https://github.com/CatMe0w/zouxian"
-  url "https://github.com/CatMe0w/zouxian/releases/download/v0.1.0/zouxian.sh"
-  sha256 "c8d290fe673d696dac8e4f2e19a3daabe3509246c713ba234c603ab627d2685b"
+  url "https://github.com/CatMe0w/zouxian/releases/download/v0.2.0/zouxian.sh"
+  sha256 "e04c163f000b8acd623daa9ab728e1a73507709d99e1089263dfbe62150e0a8e"
   license "MIT"
   head "https://github.com/CatMe0w/zouxian.git", branch: "master"
 
@@ -13,8 +13,9 @@ class Zouxian < Formula
   def caveats
     <<~EOS
       To ensure zouxian works correctly, please follow these steps:
-        - Make sure Xcode is installed and has been run at least once.
         - Disable SIP debugging restrictions by running the `csrutil enable --without debug` command in Recovery mode.
+        - For Apple Intelligence: A non-China Apple ID signed in.
+        - For Xcode Predictive Code Completion: Make sure Xcode is installed and has been run at least once.
     EOS
   end
 
